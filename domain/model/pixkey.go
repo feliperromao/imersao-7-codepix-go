8031d9a365dd7c53522d659fde081da14085da61
feliperromao <feliperomaocad@gmail.com>
2022-04-06 21:15:58 -0300

    feat: :hammer: create pixkey model

---
+++ domain/model/pixkey.go
@@ -0,0 +1,55 @@
+package model
+
+import (
+	"errors"
+	"time"
+
+	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
+)
+
+type PixKey struct {
+	Base      `valid:"required"`
+	Account   *Account ` valid:"-"`
+	AccountID string   `json:"account_id" valid:"notnull"`
+	Kind      string   `json:"kind" valid:"notnull"`
+	Key       string   `json:"key" valid:"notnull"`
+	Status    string   `json:"status" valid:"notnull"`
+}
+
+func (pixKey *PixKey) isValid() error {
+	_, err := govalidator.ValidateStruct(pixKey)
+
+	if pixKey.Kind != "email" && pixKey.Kind != "cpf" {
+		return errors.New("Invalid type of key")
+	}
+
+	if pixKey.Status != "active" && pixKey.Status != "inactive" {
+		return errors.New("Invalid status")
+	}
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func NewPixKey(Kind string, account *Account, key string) (*PixKey, error) {
+	pixKey := PixKey{
+		Kind: Kind,
+		Account: account,
+		Key: key,
+		Status: "active",
+	}
+
+	pixKey.ID = uuid.NewV4().String()
+	pixKey.CreatedAt = time.Now()
+
+	err := pixKey.isValid()
+	if err != nil {
+		return nil, err
+	}
+
+	return &pixKey, nil
+}
