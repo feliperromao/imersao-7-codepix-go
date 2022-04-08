02cea30decd196f9eae699501c7195d14ae30f37
feliperromao <feliperomaocad@gmail.com>
2022-04-08 20:29:12 -0300

    feat: :construction: implement TransactionRepositoryDb

---
+++ infra/repository/transaction.go
@@ -0,0 +1,48 @@
+package repository
+
+import (
+	"fmt"
+
+	"github.com/feliperromao/imersao-7-codepix-go/domain/model"
+	"github.com/jinzhu/gorm"
+)
+
+// type TransactionRepositoryInterface interface {
+// 	Register(t *Transaction) error
+// 	Save(t *Transaction) error
+// 	Find(id string) (*Transaction, error)
+// }
+
+
+type TransactionRepositoryDb struct {
+	Db *gorm.DB
+}
+
+func (r *TransactionRepositoryDb) Register(t *model.Transaction) error {
+	err := r.Db.Create(t).Error
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *TransactionRepositoryDb) Save(t *model.Transaction) error {
+	err := r.Db.Save(t).Error
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *TransactionRepositoryDb) Find(id string) (*model.Transaction, error) {
+	var t model.Transaction
+	r.Db.Preload("AccountFrom.Bank").First(&t, "id = ?", id)
+
+	if t.ID == "" {
+		return nil, fmt.Errorf("transaction not found")
+	}
+
+	return &t, nil
+}
